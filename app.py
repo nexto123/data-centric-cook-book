55a77268fcfac5142099ffb54defbba4b165f2db
ernest <pristowel@gmail.com>
2019-02-26 10:26:29 +0000

    amended like logic in index file

--- app.py
+++ app.py
@@ -5,16 +5,21 @@ from flask_pymongo import PyMongo, pymongo
 from multiprocessing import Value
 from bson.objectid import ObjectId
 
+
 app = Flask(__name__)
 app.secret_key = 'Ernesto_papa'
 app.config["MONGO_DBNAME"] = "cook_book"
 app.config["MONGO_URI"] = "mongodb://admin:dimension123@ds125574.mlab.com:25574/cook_book"
 
+
 class ProductionConfig():
     SESSION_COOKIE_NAME = 'session name'
     SESSION_PERMANENT = True
+    
 
 mongo = PyMongo(app)
+mod = Blueprint('recipes', __name__)
+
 
 @app.route('/')
 @app.route("/home", methods=['POST','GET'])
@@ -45,6 +50,21 @@ def home():
     #pagination is parsed into the template to be displayed
     return render_template('index.html', recipes = recipes, pagination = pagination)
 
+    
+@app.route('/likes/<recipe_id>')
+def likes(recipe_id):
+    new_recipe = mongo.db.recipes.find_one({"_id":ObjectId(recipe_id)})
+    figure = int(new_recipe['likes'])
+    like_recipe = mongo.db.recipes
+    like_recipe.update({"_id":ObjectId(recipe_id)},
+        {"$inc":
+            {
+            "likes": 1
+             }
+        },upsert=False, multi=False)
+        
+    return redirect(url_for('home'))
+
 
 # this route displays recipes in the database
 @app.route('/add_recipe')
@@ -53,16 +73,15 @@ def add_recipe():
     categories = mongo.db.categories.find())
     
     
-    
+
 # accepts requests from add recipe page and inserts recipes into db
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
     recipes = mongo.db.recipes
     recipes.insert_one(request.form.to_dict())
-    return redirect(url_for('home'))    
-
-
-
+    return redirect(url_for('home'))
+    
+    
 # this route edits recipes and is sent to the update route    
 @app.route('/edit_recipe/<recipe_id>')
 def edit_recipe(recipe_id):
@@ -70,8 +89,8 @@ def edit_recipe(recipe_id):
     categories = mongo.db.categories.find()
     return render_template('edit_recipe.html', new_recipe = new_recipe, 
     categories=categories)
-
-
+    
+    
 # this routes updates the recipes from the edit route
 @app.route('/update_recipe/<recipe_id>', methods=["POST"])
 def update_recipe(recipe_id):
@@ -87,16 +106,16 @@ def update_recipe(recipe_id):
     })
     return redirect(url_for('home'))
     
-    
-
 # this route deletes the recipe     
 @app.route('/delete_recipe/<recipe_id>')
 def delete_recipe(recipe_id):
     mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
     return redirect(url_for('home'))  
         
-    
-
+# category page
+@app.route('/add_categories')
+def add_category():
+    return render_template('add_categories.html')
 
 #this route displays categories in the database
 @app.route('/get_categories')
@@ -104,25 +123,34 @@ def get_categories():
     return render_template('categories.html',
     categories = mongo.db.categories.find())
     
+ #this route inserts categories   
+@app.route('/insert_category', methods=['POST'])
+def insert_category():
+    category = mongo.db.categories
+    cat_insert = {'category_name': request.form['category_name']}
+    category.insert_one(cat_insert)
+    return redirect(url_for('get_categories'))
     
+#this route edits categories     
+@app.route('/edit_category/<category_id>')
+def edit_category(category_id):
+    return render_template('editcategory.html',
+    category=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))    
+
 #this routes deletes categories
 @app.route('/delete_categories/<category_id>')
 def delete_categories(category_id):
     mongo.db.categories.remove({'_id': ObjectId(category_id)})
     return redirect(url_for('get_categories'))   
-    
-    
-    
-    
+
 # search route for categories
 @app.route('/list_recipes/<category_name>')
 def list_recipes(category_name):
     #search by category_name parsed from the string query 
     recipes = mongo.db.recipes.find({"category_name":category_name})
-    return render_template('list_recipes.html', recipes=recipes)   
+    return render_template('list_recipes.html', recipes=recipes)
+        
     
-    
-
 #login page route
 @app.route('/login')
 def login_page():
@@ -133,14 +161,9 @@ def login_page():
 @app.route('/session_loggout')
 def session_logout():
     session.pop('username', None)
-    return redirect(url_for('home'))    
-
-
-
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
         debug=True)
-        
-        
